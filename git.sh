829bf2d193b10a982b245a5230d4cdc2836045a7
apoczekalewicz <Artur@Poczekalewicz.net>
2022-02-01 22:50:08 +0100

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,4 @@
+  git config --global credential.helper cache
+  git config --global credential.helper "cache --timeout=8640000"
+  git config --global user.name ap
+  git config --global user.email ap@localhost
